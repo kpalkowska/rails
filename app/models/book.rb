5b7f4f4c255e8bd1e4bb6dd698f2d0e4dee0cd02
Klaudia <kpalkowska@sigma.ug.edu.pl>
2016-04-05 20:07:57 +0200

    changes

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
+
+	validates :name, :description, :price, :author_id, :img_url, presence: true
+	validates :name, :description, length: { minimum: 3 }
+	validates :price, length: { minimum: 2 }, numericality: true
 end
