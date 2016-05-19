11f40a119bc3103ed065d84e44e1a476fd2b79b9
kpalkowska <kpalkowska@sigma.ug.edu.pl>
2016-05-19 23:51:14 +0200

    Update book.rb

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
-
+	default_scope { order('id ASC') }
 	validates :name, :description, :price, :author_id, :img_url, presence: true
 	validates :name, :description, length: { minimum: 3 }
 	validates :price, length: { minimum: 2 }, numericality: true
